9a85af2fda58291bb006b165f7429305715af430
ZLTJohn <johncastle248@gmail.com>
2020-06-27 00:26:41 -0700

    4.6-4.10

--- 4/7.c
+++ 4/7.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+    //freopen("data.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
     int n = 10;
     int m;
     int numbers[10];
     int i;
     int max;
     int inter;
-    // ¶ÁÈë¸ø¶¨µÄÊı×Ö
-    for (i = 0; i < n; i++) {
+    // è¯»å…¥ç»™å®šçš„æ•°å­—
+    for (i = 0; i < n; i++)
+    {
         scanf("%d", &numbers[i]);
     }
-    for(i = 0; i <= 9; i++){
-        for(m = i; m <= 9; m++){
-            max = m;
-            if(numbers[max] < numbers[m+1]){
-                max = m + 1;
+    for (i = 0; i <= 9; i++)
+    {
+        max = i;
+        for (m = i; m <= 9; m++)
+        {
+            if (numbers[max] < numbers[m])
+            {
+                max = m;
             }
         }
         inter = numbers[i];
         numbers[i] = numbers[max];
         numbers[max] = inter;
     }
-    
-    for(i = 0; i <= 9; i++){
+
+    for (i = 0; i <= 9; i++)
+    {
         printf("%d", numbers[i]);
-        if(i != 9){
+        if (i != 9)
+        {
             printf(" ");
         }
     }
