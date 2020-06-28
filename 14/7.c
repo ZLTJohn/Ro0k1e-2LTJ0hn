7747b7d0cfd1b3ea001052a8dacfb639b1027635
ZLTJohn <johncastle248@gmail.com>
2020-06-27 20:09:39 -0700

    Finish 14.6-14.10

--- 14/7.c
+++ 14/7.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int i,j,n;
-    sacnf("%d",&n);
-    for(i=1;i<=n;i++)
-        for(j=1,j<=2*(n-i+1)-1,j++)
-            if(j%2==0)
+    int i, j, n;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= 2 * (n - i + 1) - 1; j++)
+            if (j % 2 == 0)
                 printf(" ");
             else
-                printf("%d",n-i+1);
+                printf("%d", n - (j + 1) / 2 + 2 - i);
+        printf("\n");
+    }
     return 0;
 }
--- 14/8.c
+++ 14/8.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
-int main() 
+int main()
 {
     int shuru;
     int geshu;
     int hangshu;
     int shuru2;
-    scanf("%d",&shuru);
+    scanf("%d", &shuru);
     shuru2 = shuru;
     //
-    for( hangshu = 1 ; hangshu <= shuru; hangshu ++)
+    for (hangshu = 1; hangshu <= shuru; hangshu++)
     {
-        printf("\n");
+        if (hangshu != 1)
+            printf("\n");
         shuru2 = shuru - hangshu + 1;
-        for(geshu = 1 ; geshu <= shuru ; geshu ++)
+        for (geshu = 1; geshu <= shuru; geshu++)
         {
-            printf("%d",shuru2);
-            if(shuru2 > 1)
+            printf("%d", shuru2);
+            if (shuru2 > 1)
             {
                 printf(" ");
             }
-            if(shuru2 == 1)
+            if (shuru2 == 1)
             {
                 break;
             }
-            shuru2 --;
+            shuru2--;
         }
     }
     return 0;
