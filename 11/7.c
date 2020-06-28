7e2547b8cd2a658be88c48e27a4c9e211e644df5
ZLTJohn <johncastle248@gmail.com>
2020-06-27 19:50:12 -0700

    Finish 11.6-11.10

--- 11/7.c
+++ 11/7.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-    char str[500], fs[32], arr[32], bits[32];
+int main()
+{
+    //freopen("data.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+    char str[505], fs[35];
+    int arr[32], bits[32];
     int i;
     memset(arr, 0, sizeof(arr));
     memset(fs, '\0', sizeof(fs));
     scanf("%s", str);
     for (i = 0; i < strlen(str); i++)
     {
-        if (str[i] == '\n') break;
-        arr[i % 32] += str[i];
+        if (str[i] == '\n')
+            break;
+        arr[(i + 1) % 32] += str[i];
     }
     for (i = 0; i < 32; i++)
     {
