964905c8ebfb2d9bfd9003c9116202ab5d0354ae
ZLTJohn <johncastle248@gmail.com>
2020-06-27 19:03:05 -0700

    Finish 9.6-9.10

--- 9/10.c
+++ 9/10.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-int f(int n){
-    if(n<=3){return 1;}
-    else{return f(n-2)+f(n-3);}
+int f(int n)
+{
+    if (n <= 3)
+    {
+        return n != 1;
+    }
+    else
+    {
+        return f(n - 2) + f(n - 3);
+    }
 }
-int main() {
+int main()
+{
+    //freopen("data.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
     int n;
     scanf("%d", &n);
     printf("%d", f(n));
--- 9/8.c
+++ 9/8.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
-int main() {
+int c(int n, int m)
+{
+    if (n > m / 2)
+        n = m - n;
+    long long int ret = 1, i = 0;
+    for (i = m - n + 1; i <= m; i++)
+        ret *= i;
+    for (i = 1; i <= n; i++)
+        ret /= i;
+    return ret;
+}
+int main()
+{
+    //freopen("data.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
     int n;
-    scanf("%d",&n);
-    int i=0;
+    scanf("%d", &n);
+    int i = 0;
     int x;
     int y;
-    int m=1;
-    int b=1;
-    for(x = 0; x <= n / 2; x++){
-        for(y = 0; y <= n / 3; y++){
-            if(x * 2 + y * 3 == n){
-                i++;
+    int m = 1;
+    int b = 1;
+    for (x = 0; x <= n / 2; x++)
+    {
+        for (y = 0; y <= n / 3; y++)
+        {
+            if (x * 2 + y * 3 == n)
+            {
+                i += c(x, x + y);
             }
         }
     }
-    printf("%d",i);
+    printf("%d", i);
     return 0;
 }
